83e5faea3f1b7e8332952aaacc0d8eb12d9c83bf
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-01-17 13:29:55 -0500

    Created Tocken Factory
    this is used to extract the authorization token for user's credentials,
    roles, and other information that is required before creating an
    authorization token that will consumed by webapi server.

---
+++ Skillset/SkillsetClient/Controllers/TokenFactory.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using SkillsetClient.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillsetClient.Controllers
+{
+    public class TokenFactory
+    {
+        private List<Claim> _claims;
+
+        #region "Public Methods"
+        public List<Claim> ExtractToken(String token)
+        {
+            List<Claim> claims = new List<Claim>();
+
+            _claims= new List<Claim>();
+
+            var jwtToken = new JwtSecurityToken(token);
+            
+            var names = jwtToken.Claims.
+                Where(x => x.Type==ClaimTypes.Name);
+            
+            var roles = jwtToken.Claims.
+                Where(x => x.Type == ClaimTypes.Role);
+
+            var urls = jwtToken.Claims.
+                Where(x => x.Type == ClaimTypes.Uri);
+
+            var idS = jwtToken.Claims.
+                Where(x => x.Type == ClaimTypes.Sid);
+
+            var firstNames = jwtToken.Claims.
+                Where(x => x.Type == ClaimTypes.GivenName);
+
+            var surnames = jwtToken.Claims.
+                Where(x => x.Type == ClaimTypes.Surname);
+
+            AddToClaim(names);
+            AddToClaim(roles);
+            AddToClaim(urls);
+
+            claims = _claims;
+
+            return claims;
+        }
+
+        public string GenerateAuthorizationToken(List<Claim> claims)
+        {
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(
+                    Startup.Configuration["WebApiServer:IssuerSigningKey"]);
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: Startup.Configuration["WebApiServer:ValidIssuer"],
+                audience: Startup.Configuration["WebApiServer:ValidAudience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(30),
+                signingCredentials: creds
+            );
+
+            var myToken = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return myToken;
+        }
+
+        #endregion
+
+        #region "Private Methods"
+
+        private void AddToClaim(IEnumerable<Claim> claims)
+        {
+            foreach (var claim in claims)
+            {
+                _claims.Add(claim);
+            }
+        }
+
+        #endregion   
+    }
+}
