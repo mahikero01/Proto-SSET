4fa1563ef1806c70bbdeb30ae75c733222adc604
Sarmiento <sarmife@prd.manulifeusa.com>
2018-01-17 14:30:14 -0500

    Done US22276

---
+++ Skillset/SkillsetAPI/Entities/SetUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkillsetAPI.Entities
+{
+    public class SetUser
+    {
+        [Key]
+        [MaxLength(25)]
+        public string user_id { get; set; }
+    }
+}
--- Skillset/SkillsetAPI/Startup.cs
+++ Skillset/SkillsetAPI/Startup.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace SkillsetAPI
 {
@@ -18,11 +21,29 @@ namespace SkillsetAPI
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public static IConfiguration Configuration { get; private set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = "http://localhost:60812",
+                    ValidAudience = "http://localhost:59059/",
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("03fb1760-a45f-4473-bed4-aab1e8d7e87a")),
+                };
+            });
+
+            var connectionString = Startup.Configuration["connectionStrings:cityInfoDBConnectionString"];
+            //services.AddDbContext<CityInfoContext>(o => o.UseSqlServer(connectionString));
+
             services.AddMvc();
         }
 
