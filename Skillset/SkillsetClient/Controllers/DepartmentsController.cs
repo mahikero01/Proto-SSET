c29a6f2a53ce915755a546994ad54a1ea0200c34
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-01-18 15:04:12 -0500

    Controller fixing
    Controllers must be in plural.
    will remove the incorrect controllernames

---
+++ Skillset/SkillsetClient/Controllers/DepartmentsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SkillsetClient.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Departments")]
+    public class DepartmentsController : Controller
+    {
+        // GET: api/Departments
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Departments/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/Departments
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Departments/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Skillset/SkillsetClient/Controllers/LocationsController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SkillsetClient.Models;
+
+namespace SkillsetClient.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Locations")]
+    public class LocationsController : Controller
+    {
+        // GET: api/Locations
+        private WebApiAccess _webApiAccess;
+
+        public LocationsController()
+        {
+            _webApiAccess = new WebApiAccess("Locations");
+        }
+        // GET: api/Location
+        [HttpGet]
+        public async Task<SS_Locations[]> Get()
+        {
+            _webApiAccess.AssignAuthorization(HttpContext.Session.GetString("apiToken"));
+            var skillsets = await _webApiAccess.GetRequest();
+            return JsonConvert.DeserializeObject<SS_Locations[]>(skillsets.ToString());
+        }
+
+        // GET: api/Location/5
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<SS_Locations> Get(int id)
+        {
+            _webApiAccess.AssignAuthorization(HttpContext.Session.GetString("apiToken"));
+            var skillsets = await _webApiAccess.GetRequest();
+            return JsonConvert.DeserializeObject<SS_Locations>(skillsets.ToString());
+        }
+
+        // POST: api/Location
+        [HttpPost]
+        public async Task<SS_Locations> Post([FromBody]SS_Locations location)
+        {
+            _webApiAccess.AssignAuthorization(HttpContext.Session.GetString("apiToken"));
+            var content = JsonConvert.SerializeObject(location);
+
+            var skillsets = await _webApiAccess.PostRequest(content);
+            return JsonConvert.DeserializeObject<SS_Locations>(skillsets.ToString());
+        }
+
+        // PUT: api/Location/5
+        [HttpPut("{id}")]
+        public async Task<SS_Locations> Put(int id, [FromBody]SS_Locations skillset)
+        {
+            _webApiAccess.AssignAuthorization(HttpContext.Session.GetString("apiToken"));
+            var content = JsonConvert.SerializeObject(skillset);
+
+            var skillsets = await _webApiAccess.PutRequest(id.ToString(), content);
+            return JsonConvert.DeserializeObject<SS_Locations>(skillsets.ToString());
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public async Task<bool> Delete(int id)
+        {
+            _webApiAccess.AssignAuthorization(HttpContext.Session.GetString("apiToken"));
+
+            var skillsets = await _webApiAccess.DeleteRequest(id.ToString());
+            return skillsets;
+        }
+    }
+}
