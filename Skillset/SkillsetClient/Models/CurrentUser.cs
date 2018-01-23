2e2fdc36296d66e90ffc85d1f56f64fe85c94b74
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-01-23 09:20:36 -0500

    Changed Model for Associate

--- Skillset/SkillsetClient/Models/CurrentUser.cs
+++ Skillset/SkillsetClient/Models/CurrentUser.cs
@@ -7,6 +7,7 @@ namespace SkillsetClient.Models
 {
     public class CurrentUser
     {
+        public string UserID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Role { get; set; }
--- Skillset/SkillsetClient/Models/SS_Associates.cs
+++ Skillset/SkillsetClient/Models/SS_Associates.cs
@@ -1,14 +1,17 @@
-﻿namespace SkillsetClient.Models
+﻿using System;
+
+namespace SkillsetClient.Models
 {
     public class SS_Associates
     {
         public int AssociateID { get; set; }
-        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string UserID { get; set; }
         public string PhoneNumber { get; set; }
         public bool VPN { get; set; }
         public short DepartmentID { get; set; }
         public int LocationID { get; set; }
-        public System.DateTime UpdatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
         public bool IsActive { get; set; }
     }
 }
